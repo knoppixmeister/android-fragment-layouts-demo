1ad378e83c32211cbf28fd38c2d870a661bb4b39
knoppixmeister <knoppixmeister@gmail.com>
2014-01-30 20:58:21 +0200

    c

--- FragmentLayoutProba/src/com/example/fragmentlayoutproba/MainActivity.java
+++ FragmentLayoutProba/src/com/example/fragmentlayoutproba/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.fragmentlayoutproba;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
+//--------------------------
 public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
